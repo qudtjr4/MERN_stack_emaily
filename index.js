9a292f7aab58da1f297d25abe9247f69957f601c
Minseok Kim <minseokkim@Minseoks-MacBook-Pro.local>
2020-12-17 20:29:46 -0800

    added index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/greeting', (req, res) => {
-  res.send({ hi: 'there' });
+app.get('/', (req, res) => {
+  res.send({ hogu: 'Merong!!!!!' });
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(PORT);
