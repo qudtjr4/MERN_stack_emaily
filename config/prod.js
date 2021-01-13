32f27264a731419682837a4966f4828501fc700a
Minseok Kim <minseokkim@Minseoks-MacBook-Pro.local>
2021-01-13 00:37:27 -0800

    Product Version Control

---
+++ config/prod.js
@@ -0,0 +1,12 @@
+module.exports = {
+  googleClientID : process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKEY_KEY
+}
+
+//new google google
+//new mongodb
+//set up heroku env
+//git commit
+//git push heroku master
